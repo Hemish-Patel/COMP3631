43c57999c25e83ed70d8119ed1451541018b1fc2
Heish <el22hp@leeds.ac.uk>
2026-02-20 20:21:34 +0000

    completed lab 3: circle and square exercises

---
+++ lab3/lab3/exercise_2.py
@@ -0,0 +1,72 @@
+import threading
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+from rclpy.exceptions import ROSInterruptException
+import signal
+
+
+class FirstWalker(Node):
+    def __init__(self):
+        super().__init__('firstwalker')
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.rate = self.create_rate(10)  # 10 Hz
+
+    def walk_forward(self):
+        desired_velocity = Twist()
+        desired_velocity.linear.x = 0.2  # Forward with 0.2 m/s
+        desired_velocity.angular.z = 0.0
+          
+
+        for _ in range(35):  # Stop for a brief moment
+            self.publisher.publish(desired_velocity)
+            self.rate.sleep()
+            
+    def spin_anticlockwise(self):
+        desired_velocity = Twist()
+        desired_velocity.linear.x = 0.0  # No linear movement
+        desired_velocity.angular.z = 1.57  
+        for _ in range(15):  # Stop for a brief moment
+            self.publisher.publish(desired_velocity)
+            self.rate.sleep()
+
+    def walk_backward(self):
+        desired_velocity = Twist()
+        desired_velocity.linear.x = -0.2  # Backward with 0.2 m/s
+        desired_velocity.angular.z = -0.2
+        for _ in range(30):  # Stop for a brief moment
+            self.publisher.publish(desired_velocity)
+            self.rate.sleep()
+
+    def stop(self):
+        desired_velocity = Twist()
+        desired_velocity.linear.x = 0.0  # Send zero velocity to stop the robot
+        desired_velocity.angular.z = 0.0
+        self.publisher.publish(desired_velocity)
+
+def main():
+    def signal_handler(sig, frame):
+        first_walker.stop()
+        rclpy.shutdown()
+
+    rclpy.init(args=None)
+    first_walker = FirstWalker()
+
+    signal.signal(signal.SIGINT, signal_handler)
+    thread = threading.Thread(target=rclpy.spin, args=(first_walker,), daemon=True)
+    thread.start()
+
+    try:
+        while rclpy.ok():
+            while True:
+                first_walker.walk_forward()
+                first_walker.spin_anticlockwise()
+            #first_walker.walk_backward()
+    except ROSInterruptException:
+        pass
+
+
+if __name__ == "__main__":
+    main()
+
+
